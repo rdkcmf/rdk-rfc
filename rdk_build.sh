9e1831935161570e8259d661b544a7763ddb2362
jkkuriakose <josekutty_kuriakose@cable.comcast.com>
2018-02-28 17:19:40 -0500

    RDK-21479 rdk build scripts
    Reason for change: RFC tool needs to be part of RNG devices also. This change adds
    necessary changes for RDK build
    Test Procedure: Make sure rfc utilities are present in all builds
    Risks: Low
    
    Change-Id: I299a52f19b5399e86424c1821267431ce85c747e
    Signed-off-by: jkkuriakose <josekutty_kuriakose@cable.comcast.com>

---
+++ rdk_build.sh
@@ -0,0 +1,222 @@
+#!/bin/bash
+#
+# ============================================================================
+# RDK MANAGEMENT, LLC CONFIDENTIAL AND PROPRIETARY
+# ============================================================================
+# This file (and its contents) are the intellectual property of RDK Management, LLC.
+# It may not be used, copied, distributed or otherwise  disclosed in whole or in
+# part without the express written permission of RDK Management, LLC.
+# ============================================================================
+# Copyright (c) 2014 RDK Management, LLC. All rights reserved.
+# ============================================================================
+#
+
+#######################################
+#
+# Build Framework standard script for
+#
+# MediaFramework component
+
+# use -e to fail on any shell issue
+# -e is the requirement from Build Framework
+set -e
+
+# default component name
+export RDK_COMPONENT_NAME=${RDK_COMPONENT_NAME-`basename $RDK_SOURCE_PATH`}
+
+# default PATHs - use `man readlink` for more info
+# the path to combined build
+export RDK_PROJECT_ROOT_PATH=${RDK_PROJECT_ROOT_PATH-`readlink -m ..`}
+export COMBINED_ROOT=$RDK_PROJECT_ROOT_PATH
+
+# path to build script (this script)
+export RDK_SCRIPTS_PATH=${RDK_SCRIPTS_PATH-`readlink -m $0 | xargs dirname`}
+
+# path to components sources and target
+export RDK_SOURCE_PATH=${RDK_SOURCE_PATH-$RDK_SCRIPTS_PATH}
+export RDK_TARGET_PATH=${RDK_TARGET_PATH-$RDK_SOURCE_PATH}
+
+# fsroot and toolchain (valid for all devices)
+export RDK_FSROOT_PATH=${RDK_FSROOT_PATH-`readlink -m $RDK_PROJECT_ROOT_PATH/sdk/fsroot/ramdisk`}
+export RDK_TOOLCHAIN_PATH=${RDK_TOOLCHAIN_PATH-`readlink -m $RDK_PROJECT_ROOT_PATH/sdk/toolchain/staging_dir`}
+
+
+if [[ "$RDK_PLATFORM_SOC" == "broadcom" ]] ; then
+    export BUILDS_DIR=$RDK_PROJECT_ROOT_PATH
+    export DEFAULT_HOST=mipsel-linux
+fi
+
+export IARM_PATH=$BUILDS_DIR/iarmbus
+export OPENSRC=$RDK_PROJECT_ROOT_PATH/opensource
+export cjson_CFLAGS='-I${OPENSRC}/lib/cjson/include'
+export cjson_LIBS='-L${OPENSRC}/lib -lcjson'
+
+# component-specific vars
+export FSROOT=${RDK_FSROOT_PATH}
+export TOOLCHAIN_DIR=${RDK_TOOLCHAIN_PATH}
+
+#Set the Path and Export Variables
+if [ $RDK_PLATFORM_SOC = "intel" ]; then
+    export CC=$RDK_PROJECT_ROOT_PATH/sdk/toolchain/staging_dir/bin/i686-cm-linux-gcc
+    export CXX=$RDK_PROJECT_ROOT_PATH/sdk/toolchain/staging_dir/bin/i686-cm-linux-g++
+    export CROSS_TOOLCHAIN=$TOOLCHAIN_DIR
+elif [ $RDK_PLATFORM_SOC = "broadcom" ]; then
+    echo "building for ${RDK_PLATFORM_DEVICE}..."
+    export WORK_DIR=$BUILDS_DIR/work${RDK_PLATFORM_DEVICE^^}
+    . $WORK_DIR/../build_scripts/setBCMenv.sh
+    
+    CROSS_COMPILE=mipsel-linux
+    export CC=$CROSS_COMPILE-gcc
+    export CXX=$CROSS_COMPILE-g++
+fi
+
+# parse arguments
+INITIAL_ARGS=$@
+
+
+function usage()
+{
+    set +x
+    echo "Usage: `basename $0` [-h|--help] [-v|--verbose] [action]"
+    echo "    -h    --help                  : this help"
+    echo "    -v    --verbose               : verbose output"
+    echo
+    echo "Supported actions:"
+    echo "      configure, clean, build (DEFAULT), rebuild, install"
+}
+
+# options may be followed by one colon to indicate they have a required argument
+if ! GETOPT=$(getopt -n "rdk_build.sh" -o hvk -l help,verbose -- "$@")
+then
+    usage
+    exit 1
+fi
+
+eval set -- "$GETOPT"
+
+while true; do
+  case "$1" in
+    -h | --help ) usage; exit 0 ;;
+    -v | --verbose ) set -x ;;
+    -- ) shift; break;;
+    * ) break;;
+  esac
+  shift
+done
+
+ARGS=$@
+
+
+# component-specific vars
+export FSROOT=$RDK_FSROOT_PATH
+
+
+# functional modules
+
+function configure()
+{
+    pd=`pwd`
+    cd ${RDK_SOURCE_PATH}
+    autoreconf -f -i
+    echo "  CONFIG_MODE = $CONFIG_MODE"
+    
+    configure_options=" "
+    if [ "x$DEFAULT_HOST" != "x" ]; then
+	echo "DEFAULT_HOST is $DEFAULT_HOST"
+        configure_options="--host $DEFAULT_HOST"
+    else
+	echo "NO DEFAULT_HOST is defined"
+    fi
+
+    configure_options="$configure_options --enable-shared --with-pic"
+    generic_options="$configure_options"
+    
+    extraenablers="--enable-rfctool"
+
+    if [ "$RDK_PLATFORM_DEVICE" != "rng150" ] ; then
+        echo "Add tr181Set tool	"
+        extraenablers="$extraenablers --enable-tr181set"
+    fi
+     
+    if [ "$RDK_PLATFORM_SOC" = "stm" ];then
+        ./configure --prefix=/usr $configure_options $extraenablers
+    else
+        ./configure --prefix=${RDK_FSROOT_PATH}/usr $configure_options $extraenablers
+    fi
+    cd $pd
+}
+
+function clean()
+{
+    pd=`pwd`
+    dName="${RDK_SOURCE_PATH}"
+    cd $dName
+    if [ -f Makefile ]; then
+        make distclean
+    fi
+    rm -f configure;
+    rm -rf aclocal.m4 autom4te.cache config.log config.status libtool
+    find . -iname "Makefile.in" -exec rm -f {} \; 
+    if [ "$RDK_PLATFORM_SOC" = "stm" ];then
+        find . -name Makefile | grep -v "aes" | xargs rm -rf
+    else
+        find . -iname "Makefile" -exec rm -f {} \; 
+    fi
+    ls cfg/* | grep -v "Makefile.am" | xargs rm -f
+    cd $pd
+}
+
+function build()
+{
+    cd ${RDK_SOURCE_PATH}
+    make
+    #make install
+    #make -C qamsource
+    #make -C snmp
+}
+
+function rebuild()
+{
+    clean
+    configure
+    build
+}
+
+function install()
+{
+    cd ${RDK_SOURCE_PATH}
+
+    if [ "$RDK_PLATFORM_SOC" = "stm" ];then
+       make install DESTDIR=${RDK_FSROOT_PATH}
+    else
+       make install
+    fi
+    cp utils/rfctool $FSROOT/usr/bin
+    if [ "x${RDK_PLATFORM_DEVICE^^}" != "xRNG150" ]; then
+        cp utils/tr181Set $FSROOT/usr/bin
+    fi
+}
+
+
+# run the logic
+
+#these args are what left untouched after parse_args
+HIT=false
+
+for i in "$ARGS"; do
+    case $i in
+        configure)  HIT=true; configure ;;
+        clean)      HIT=true; clean ;;
+        build)      HIT=true; build ;;
+        rebuild)    HIT=true; rebuild ;;
+        install)    HIT=true; install ;;
+        *)
+            #skip unknown
+        ;;
+    esac
+done
+
+# if not HIT do build by default
+if ! $HIT; then
+  rebuild
+fi
